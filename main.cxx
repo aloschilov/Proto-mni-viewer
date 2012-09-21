e4cf3b56591e274e7ebd286ce5a8eafc7d137de1
Alexander Loschilov <aloschil@gmail.com>
2012-09-21 19:53:26 +0400

    ADD: debug messages to debug.txt

--- main.cxx
+++ main.cxx
@@ -2,8 +2,37 @@
 
 #include "mainwindow.h"
 
+void myMessageOutput(QtMsgType type, const char *msg)
+{
+    FILE *file;
+
+    if((file = fopen("debug.txt", "a+")) == NULL)
+    {
+        return;
+    }
+
+    switch (type) {
+    case QtDebugMsg:
+        fprintf(file, "Debug: %s\n", msg);
+        break;
+    case QtWarningMsg:
+        fprintf(file, "Warning: %s\n", msg);
+        break;
+    case QtCriticalMsg:
+        fprintf(file, "Critical: %s\n", msg);
+        break;
+    case QtFatalMsg:
+        fprintf(file, "Fatal: %s\n", msg);
+        abort();
+    }
+
+    fclose(file);
+}
+
 int main(int argc, char** argv)
 {
+    qInstallMsgHandler(myMessageOutput);
+
     QVTKApplication app(argc, argv);
 
     QCoreApplication::setOrganizationName("G");
